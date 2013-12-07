70d669a61d72f4c60722c8cccd7b787c024e4243
Victor Padilla <vector9x@cedesoft09.(none)>
2013-12-07 13:17:48 -0500

    incluir tema de bootstrap; inicio de interfaz ruat

--- application/controllers/prueba.php
+++ application/controllers/prueba.php
@@ -1,6 +1,6 @@
 <?php
 class Prueba extends CI_Controller {
-    public function test() 
+    public function test($nombre="Foraneo") 
     {
         
         //$this->load->database();
@@ -9,15 +9,25 @@ class Prueba extends CI_Controller {
 
         //$this->load->model("new");
 
-        $res = Noticia::find_by_title("noticia 1");
+        //$res = Noticia::find_by_title("noticia 1");
 
         //var_dump($res);
 
         //var_dump($news);
 
         $this->load->spark('Twiggy/0.8.5');
-
+        //$this->twiggy
+        $this->twiggy->set("usuario", $nombre);
         $this->twiggy->template("auth/login");
         $this->twiggy->display();
     }
+
+    public function ensayo()
+    {
+        $this->load->spark('Twiggy/0.8.5');
+        $this->twiggy->template("ruat/datos_personales");
+        $this->twiggy->display();
+    }
+
+
 }
