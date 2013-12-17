2a8d27337cac7b5fc77fb2c395fb2934bb32c65e
victor <vector9x@gmail.com>
2013-12-17 11:28:22 -0500

    fix en create_user

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -441,7 +441,7 @@ class Auth extends CI_Controller {
                 //check to see if we are creating the user
                 //redirect them back to the admin page
                 $this->session->set_flashdata('message', $this->ion_auth->messages());
-                //redirect("auth", 'refresh');
+                redirect("auth", 'refresh');
             }
             else {
                 $this->data['message'] = $this->ion_auth->errors();
