95a41b4372067aa409f4b88503a2254c47cf575f
victor <vector9x@gmail.com>
2014-01-12 00:11:05 -0500

    inicializacion switch

--- assets/js/pages/domready.js
+++ assets/js/pages/domready.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     $('.datepicker.autostart').datepicker({autoclose:true, language: "es", format:'yyyy-mm-dd'});
     //$('select.select2.with-filter').select2({allowClear:true, placeholder:'-'});
     //$('select.select2.without-filter').select2({allowClear:true, placeholder:'-', minimumResultsForSearch: -1});
+    $('.switch').bootstrapSwitch();
 });
 
 var count = function(obj) {
