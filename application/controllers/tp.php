7a21863db5713df14883aecab57a51f4200f137b
Eduar Hincapie <eduar219@ymail.com>
2014-01-13 08:39:20 -0500

    Guardando/Editando parte b vtp

--- application/controllers/tp.php
+++ application/controllers/tp.php
@@ -1,77 +1,96 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Tp extends CI_Controller {
 
-    public function index($id = null)
-    {
+    public function index($id = null) {
         $id = 1;
-        
+
         $this->load->library('form_validation');
         $this->form_validation->set_error_delimiters('<div><label class="error">', '</label></div>');
-        
-        ///consulto las preguntas para cargarlas dinamicamente
-        $preguntas_c= TPCPregunta::all(array('order' => 'categoria, ordenamiento'));
-        
-        ///consulo las respuestas
+
+
+        ///consulo las respuestas B
+        $respuesta_b = TPBRespuesta::first(array(
+                    'conditions' => array('visita_id = ?', $id)
+        ));
+        $this->form_validation->set_rules("area_predio", ' ', 'required|numeric');
+        $this->form_validation->set_rules("valor_uaf", ' ', 'required|numeric');
+        $this->form_validation->set_rules("tipo_productor_uaf", ' ', 'required|is_natural');
+        $this->form_validation->set_rules("clasificacion_productor_uaf", ' ', 'required|is_natural');
+
+
+        ///consulto las preguntas C para cargarlas dinamicamente
+        $preguntas_c = TPCPregunta::all(array('order' => 'categoria, ordenamiento'));
+
+        ///consulo las respuestas C
         $respuestas_c = TPCRespuesta::all(array(
-                'conditions' => array('visita_id = ?', $id)
-            ));
+                    'conditions' => array('visita_id = ?', $id)
+        ));
         ///acomodo las respuestas con la pregunta como llave
         $respuestas_c_aux = array();
         foreach ($respuestas_c as $obj)
             $respuestas_c_aux[$obj->pregunta_c_id] = $obj;
-        
-        
-        ///acomodo las preguntas por categoria
-        $preguntas_ingresos = $preguntas_egresos = $preguntas_activos = $preguntas_totales = array();        
+
+        ///acomodo las preguntas C por categoria
+        $preguntas_ingresos = $preguntas_egresos = $preguntas_activos = $preguntas_totales = array();
         foreach ($preguntas_c as $obj) {
-            
-            $this->form_validation->set_rules("pregunta_c_{$obj->id}", ' ', 'required|numeric');            
+
+            $this->form_validation->set_rules("pregunta_c_{$obj->id}", ' ', 'required|numeric');
             $objAux = $obj->to_array();
-            
-            ///Si la pregunta tiene respuesta, la agrego al objeto de la pregunta
-            if(isset($respuestas_c_aux[$obj->id]))
+
+            ///Si la pregunta C tiene respuesta, la agrego al objeto de la pregunta
+            if (isset($respuestas_c_aux[$obj->id]))
                 $objAux['respuesta_bd'] = $respuestas_c_aux[$obj->id]->valor;
-            
-            if($obj->categoria === 'A')
+
+            if ($obj->categoria === 'A')
                 $preguntas_activos[] = $objAux;
-            elseif($obj->categoria === 'B')
+            elseif ($obj->categoria === 'B')
                 $preguntas_ingresos[] = $objAux;
-            elseif($obj->categoria === 'C')
+            elseif ($obj->categoria === 'C')
                 $preguntas_egresos[] = $objAux;
-            elseif($obj->categoria === 'D')
+            elseif ($obj->categoria === 'D')
                 $preguntas_totales[] = $objAux;
         }
-        
+
         if ($this->form_validation->run()) {
-           
+
+            ///GUARDANDO RESPUESTAS B
+            $respuesta_b = ($respuesta_b) ? $respuesta_b : new TPBRespuesta;
+            $respuesta_b->visita_id = $id;
+            $respuesta_b->area_predio =$this->input->post('area_predio');
+            $respuesta_b->valor_uaf =$this->input->post('valor_uaf');
+            $respuesta_b->tipo_productor_uaf =$this->input->post('tipo_productor_uaf');
+            $respuesta_b->clasificacion_productor_uaf =$this->input->post('clasificacion_productor_uaf');
+            $respuesta_b->save();
+
+
+            ///GUARDANDO RESPUESTAS C
             foreach ($preguntas_c as $obj) {
-                
-//                var_dump($this->input->post('pregunta_c_' . $obj->id));
+
                 ///Valido si la respuesta viene por post
-                if(empty($this->input->post('pregunta_c_' . $obj->id)))
-                    continue;
-                
-                
-                $valor = $this->input->post('pregunta_c_' . $obj->id);
-                ///si estoy editando o nueva respuesta
-                $objTPCRespuesta = isset($respuestas_c_aux[$obj->id]) ? $respuestas_c_aux[$obj->id] : new TPCRespuesta;
-                $objTPCRespuesta->visita_id = $id;
-                $objTPCRespuesta->pregunta_c_id = $obj->id;
-                $objTPCRespuesta->valor = $valor;
-                $objTPCRespuesta->save();
+                if (!empty($this->input->post('pregunta_c_' . $obj->id))) {
+
+                    $valor = $this->input->post('pregunta_c_' . $obj->id);
+                    ///si estoy editando o nueva respuesta
+                    $objTPCRespuesta = isset($respuestas_c_aux[$obj->id]) ? $respuestas_c_aux[$obj->id] : new TPCRespuesta;
+                    $objTPCRespuesta->visita_id = $id;
+                    $objTPCRespuesta->pregunta_c_id = $obj->id;
+                    $objTPCRespuesta->valor = $valor;
+                    $objTPCRespuesta->save();
+                }
             }
-            
-//            die();
-            
-            
         }
-        
+
+        $this->twiggy->set('respuesta_b', $respuesta_b);
         $this->twiggy->set('preguntas_ingresos', $preguntas_ingresos);
         $this->twiggy->set('preguntas_egresos', $preguntas_egresos);
         $this->twiggy->set('preguntas_activos', $preguntas_activos);
         $this->twiggy->set('preguntas_totales', $preguntas_totales);
+
         $this->twiggy->template("vtp/tp");
         $this->twiggy->display();
     }
+
 }
