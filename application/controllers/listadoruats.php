5c138c62585dff4f45c98105f8cacb7712da0b98
victor <vector9x@gmail.com>
2014-01-14 23:13:31 -0500

    Columna Autor en listado ruats

--- application/controllers/listadoruats.php
+++ application/controllers/listadoruats.php
@@ -9,7 +9,7 @@ class ListadoRuats extends CI_Controller {
         //if($this->ion_auth->in_group("Digitador"))
         //    $ruats = Ruat::find_all_by_creador_id(current_user('id'), array('include' => array('bpa','cosecha','observacion')));
         //else
-            $ruats = Ruat::all(array('include' => array('bpa','cosecha','observacion')));
+            $ruats = Ruat::all(array('include' => array('bpa','cosecha','observacion', 'creador')));
 
         //foreach($ruats as $r) echo $r->observacion->ruta_formulario;
 
