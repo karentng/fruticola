998fc21a77e44562824e06205a7c20099f67753c
Victor Padilla <vector9x@gmail.com>
2014-01-21 15:13:41 -0500

    arreglado upload en bpa

--- application/controllers/uploads.php
+++ application/controllers/uploads.php
@@ -1,21 +1,34 @@
 <?php
 
 class Uploads extends CI_Controller {
+    public function __construct()
+    {
+        parent::__construct();
+        if(!$this->ion_auth->logged_in()) show_404();        
+        $this->load->helper('download');
+    }
+
     public function ruat($file)
     {
-        if(!$this->ion_auth->logged_in()) show_404();
-
-        $this->load->helper('download');
         $filepath = FCPATH."uploads/ruat/$file";
         force_download($file, file_get_contents($filepath));
     }
 
     public function documentos_identificacion($file)
     {
-        if(!$this->ion_auth->logged_in()) show_404();
-
-        $this->load->helper('download');
         $filepath = FCPATH."uploads/documentos_identificacion/$file";
         force_download($file, file_get_contents($filepath));
     }
+
+    public function bpa($file)
+    {
+        $filepath = FCPATH."uploads/bpa/$file";
+        force_download($file, file_get_contents($filepath));
+    }    
+
+     public function vtp($file)
+    {
+        $filepath = FCPATH."uploads/vtp/$file";
+        force_download($file, file_get_contents($filepath));
+    }    
 }
