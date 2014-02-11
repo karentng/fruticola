46ae4c5a7010838dbfc63ddc139bbd0960c4a6f4
Andrés Camilo Serna <andres.c.serna@gmail.com>
2014-02-11 00:39:57 -0500

    mejora amchart en metas; creacion ubicacion de predios 90%

---
+++ application/controllers/reportes/ubicaciongoogle.php
@@ -0,0 +1,54 @@
+<?php
+
+class Ubicaciongoogle extends CI_Controller {
+    public function __construct()
+    {
+        parent::__construct();
+        //check_profile(array("Administrador", "Coordinador", "Consultas"));
+    }
+
+    public function index()
+    {
+        /*$informes = array(
+            array('url' => 'informes/prueba', 'title' => 'Reportico', 'icon' => 'i-file-8', 'color' => 'green'),
+            array('url' => 'informes/ruatImprimible', 'title' => 'Imprimir Ruat', 'icon' => 'i-print', 'color' => 'blue'),
+            array('url' => 'informes/oto', 'title' => 'Oto Reporte', 'icon' => 'i-file-7', 'color' => 'red')
+
+        );
+        
+        $this->twiggy->set('informes', $informes);*/
+        $result = array();
+        $fincas = Finca::find('all', array('order' => 'nombre'));
+
+        foreach($fincas as $finca){
+            array_push($result, $finca->to_array());
+        }
+
+        $this->twiggy->set('fincas', json_encode($result));
+
+        ///$this->load->library('form_validation');
+        $this->crearArchivoJSON($result);
+        $this->twiggy->template("reportes/ubicaciongoogle");
+        $this->twiggy->display();
+        
+    }
+    public function crearArchivoJSON($fincas){
+        $result = array();
+        for($i = 0; $i < count($fincas); $i++){
+            $aux = array();
+            $aux['name'] = $fincas[$i]['nombre'];
+            $aux['address'] = Municipio::find_by_id($fincas[$i]['municipio'])->nombre;
+            $aux['city'] = $fincas[$i]['vereda'];
+            $aux['state'] = $fincas[$i]['sector'];
+            $aux['postal'] = "Área total: ".$fincas[$i]['area_total'];
+            $aux['phone'] = "";
+            $aux['web'] = "";
+            $aux['lat'] = $fincas[$i]['geo_latitud'];
+            $aux['lng'] = $fincas[$i]['geo_logitud'];
+            array_push($result, $aux);
+        }
+        $fp = fopen('assets/google_maps/results.json', 'w');
+        fwrite($fp, json_encode($result));
+        fclose($fp);
+    }
+}
