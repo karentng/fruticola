20cf197386195037588cd5903848f26c323b42fb
victor <vector9x@gmail.com>
2014-02-14 19:35:27 -0500

    chequeo de login en imprimible

--- application/controllers/ruatImprimible.php
+++ application/controllers/ruatImprimible.php
@@ -1,6 +1,11 @@
 <?php
 
 class RuatImprimible extends CI_Controller {
+    public function __construct()
+    {
+        parent::__construct();
+        check_profile(array("Administrador","Coordinador","Digitador","Consultas"));
+    }
     public function index($ruat_id) 
     {
     	$ruat = Ruat::find($ruat_id);
