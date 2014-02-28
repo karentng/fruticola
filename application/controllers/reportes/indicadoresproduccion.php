afc54afbeb51a6cd4b20c4991f6007b456d477c4
Eduar Hincapie <eduar219@ymail.com>
2014-02-28 16:06:31 -0500

    sadasd

--- application/controllers/reportes/indicadoresproduccion.php
+++ application/controllers/reportes/indicadoresproduccion.php
@@ -12,9 +12,19 @@ class Indicadoresproduccion extends CI_Controller {
         function to_array($model) {
             return $model->to_array();
         }
+        
+        $municipios = array_map('to_array',Municipio::find_all_by_departamento_id(30, array('select'=>'id,nombre' , 'order'=>'nombre')));
+        $renglonesProductivos = array_map('to_array',RenglonProductivo::sorted());
 
+        $this->twiggy->set('municipios', $municipios);
+        $this->twiggy->set('renglonesProductivos', $renglonesProductivos);
+        
         $this->twiggy->template("reportes/indicadoresproduccion");
         $this->twiggy->display();
+    }
+    
+    private function consultarDatos($municipio, $renglon){
+        
     }
 
 }
