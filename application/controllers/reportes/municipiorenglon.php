063ff933b5fd00167e5071ce95790a72506e98c7
Andrés Camilo Serna <andres.c.serna@gmail.com>
2014-03-02 21:19:52 -0500

    ruatxmunicipioxrenglon listo

---
+++ application/controllers/reportes/municipiorenglon.php
@@ -0,0 +1,109 @@
+<?php
+
+class MunicipioRenglon extends CI_Controller {
+    public function __construct()
+    {
+        parent::__construct();
+        //check_profile(array("Administrador", "Coordinador", "Consultas"));
+    }
+
+    public function index()
+    {
+        //$this->twiggy->set('municipios', $municipios);
+        $tabla = array();
+        $totalMunicipios = array();
+        $totalRenglon = array();
+        $total = 0;
+
+        $renglones = RenglonProductivo::sorted();
+        $municipios = Municipio::find('all', array('conditions' => array('departamento_id = ?', 30), 'order' => 'nombre'));
+
+        foreach($renglones as $renglon){
+            array_push($totalRenglon, 0);
+        }
+
+        foreach($municipios as $municipio){
+            array_push($totalMunicipios, 0);
+            $mun = array();
+            for($i = 0; $i < count($renglones) ; $i++){
+                array_push($mun, 0);
+            }
+            
+            $fincas = Finca::find('all', array('conditions' => array('municipio_id = ?', $municipio->id)));
+            $ruats = extract_prop($fincas, 'ruat_id');
+            
+            if(count($ruats) == 0){
+                array_push($tabla, $mun);
+                continue;
+            }
+            
+            foreach($ruats as $r){
+                $productor = Ruat::find('all', array('conditions' => array('id = ?', $r)))[0]->productor_id;
+                $ren = Productor::find('all', array('conditions' => array('id = ?', $productor), 'order' => 'renglon_productivo_id'))[0]->renglon_productivo_id;
+                $mun[$ren-1] += 1;
+            }
+            array_push($tabla, $mun);
+        }
+
+        for($i = 0 ; $i < count($tabla) ; $i++){
+            for($j = 0 ; $j < count($tabla[$i]) ; $j++){
+                $totalMunicipios[$i] += $tabla[$i][$j];
+                $total += $tabla[$i][$j];
+                $totalRenglon[$j] += $tabla[$i][$j];
+            }
+        }
+        /*var_dump($totalMunicipios);
+        var_dump($totalRenglon);*/
+
+        $this->twiggy->set('renglones', $renglones);
+        $this->twiggy->set('municipios', $municipios);
+        $this->twiggy->set('info', $tabla);
+        $this->twiggy->set('total', $total);
+        $this->twiggy->set('totalM', $totalMunicipios);
+        $this->twiggy->set('totalR', $totalRenglon);
+
+        $this->twiggy->set('infoJSON', json_encode($tabla));
+        $aux = array();
+        $aux2 = array();
+        foreach($renglones as $reng){
+            array_push($aux, $reng->to_array());
+        }
+        foreach($municipios as $mun){
+            array_push($aux2, $mun->to_array());
+        }
+        $this->twiggy->set('renglonesJSON', json_encode($aux));
+        $this->twiggy->set('municipiosJSON', json_encode($aux2));
+
+        $this->twiggy->template("reportes/municipiorenglon");
+        $this->twiggy->display();
+    }
+
+    public function fincasPorMunicipio(){
+        $id = $_POST['id'];
+
+        $productores = array();
+        $renglon_productivo = array();
+        $nombresMunicipio = array();
+
+        $fincas = Finca::find('all', array('conditions' => array('municipio_id = ?', $id)));
+        $result = array();
+        foreach($fincas as $finca){
+            array_push($nombresMunicipio, Municipio::find_by_id($finca->municipio_id)->to_array());
+
+            $ruat = Ruat::find_by_id($finca->ruat_id);
+            $prod = Productor::find_by_id($ruat->productor_id);
+
+            array_push($productores, $prod->nombre1 ." ". $prod->nombre2 ." ". $prod->apellido1 ." ". $prod->apellido2);
+
+            $renglon = RenglonProductivo::find_by_id($prod->renglon_productivo_id);
+
+            array_push($renglon_productivo, $renglon->descripcion);
+            array_push($result, $finca->to_array());
+        }
+        $final = array();
+        array_push($final, $result);
+        array_push($final, $renglon_productivo);
+        array_push($final, $nombresMunicipio);
+        echo json_encode($final);
+    }
+}
