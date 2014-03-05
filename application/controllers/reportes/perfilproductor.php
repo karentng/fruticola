6225437f080d4ec1676c043f8268c67b12f3e853
Karent Nathalia Narvaez <karentnathalia@gmail>
2014-03-05 02:49:18 -0500

    creación vista reporte perfil productor

--- application/controllers/reportes/perfilproductor.php
+++ application/controllers/reportes/perfilproductor.php
@@ -9,22 +9,24 @@ class Perfilproductor extends CI_Controller {
 
     public function index()
     {
-    	$data = array();
+        $data = array();
 
-        $query = "SELECT avg(:var)
-                    FROM productor
-                    INNER JOIN economia ON productor_id=productor.id
-        			";
+        $query = "SELECT avg(A.edad)
+                    FROM(
+                    SELECT DISTINCT productor, edad
+                    FROM perfil_productor
+                    ORDER BY productor) as A
+                    ";
 
-		$frecuencias_sexo = Productor::connection()->query($query);
+        $promedio_edad = Perfilproductor::connection()->query($query);
 
-    	if(check_profile(array("Administrador","Coordinador"), false)){
+        if(check_profile(array("Administrador","Coordinador"), false)){
             $this->twiggy->set('soloLectura', false);
         }else{
             $this->twiggy->set('soloLectura', true);
         }
 
-        $this->twiggy->set('sexos', $frecuencias_sexo);
+        //$this->twiggy->set('promedio_edad', $promedio_edad);
         $this->twiggy->template("reportes/perfilproductor");
         $this->twiggy->display();
     }
