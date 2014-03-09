0cde922982b6fa14a872409267017b5e34011ede
victor <vector9x@gmail.com>
2014-03-08 21:42:07 -0500

    municipio * renglon optimizado

--- application/controllers/reportes/municipiorenglon.php
+++ application/controllers/reportes/municipiorenglon.php
@@ -4,77 +4,49 @@ class MunicipioRenglon extends CI_Controller {
     public function __construct()
     {
         parent::__construct();
-        //check_profile(array("Administrador", "Coordinador", "Consultas"));
+        check_profile(array("Administrador", "Coordinador", "Consultas"));
     }
+
+
 
     public function index()
     {
-        //$this->twiggy->set('municipios', $municipios);
-        $tabla = array();
-        $totalMunicipios = array();
-        $totalRenglon = array();
         $total = 0;
 
         $renglones = RenglonProductivo::sorted();
         $municipios = Municipio::find('all', array('conditions' => array('departamento_id = ?', 30), 'order' => 'nombre'));
 
-        foreach($renglones as $renglon){
-            array_push($totalRenglon, 0);
+        $tabla = array();
+        foreach($municipios as $i => $_) 
+            foreach($renglones as $j => $_)
+                $tabla[$i][$j] = 0;
+
+        //die(json_encode($tabla));
+
+        $totalMunicipios = array_fill(0, count($municipios), 0);
+        $totalRenglones = array_fill(0, count($renglones), 0);
+
+        $indexMunicipio = array();
+        foreach($municipios as $idx => $mun) $indexMunicipio[$mun->id] = $idx;
+        $indexRenglon = array();
+        foreach($renglones as $idx => $ren) $indexRenglon[$ren->id] = $idx;
+
+        $sql = "
+            select F.municipio_id, P.renglon_productivo_id, count(R.id) as conteo
+            from ruat R join finca F on F.ruat_id=R.id
+            join productor P on P.id=R.productor_id
+            group by F.municipio_id, P.renglon_productivo_id
+        ";
+
+        $dbres = $this->db->query($sql);
+        foreach($dbres->result() as $row) {
+            $mun = $indexMunicipio[$row->municipio_id];
+            $ren = $indexRenglon[$row->renglon_productivo_id];
+            $tabla[$mun][$ren] = $row->conteo;
+            $totalRenglon[$ren] += $row->conteo;
+            $totalMunicipios[$mun] += $row->conteo;
+            $total += $row->conteo;
         }
-
-        foreach($municipios as $municipio){
-            array_push($totalMunicipios, 0);
-            $mun = array();
-            for($i = 0; $i < count($renglones) ; $i++){
-                array_push($mun, 0);
-            }
-            
-            $fincas = Finca::find('all', array('conditions' => array('municipio_id = ?', $municipio->id)));
-            $ruats = extract_prop($fincas, 'ruat_id');
-            
-            
-            if(count($ruats) == 0){
-                array_push($tabla, $mun);
-                continue;
-            }
-
-            
-            /*$productor = Ruat::find('all', array('conditions' => array('id in (?)', $ruats)));
-            $productor = extract_prop($productor, 'productor_id');
-            //$productor = $productor->productor_id;
-            $ren = Productor::find('all', array('conditions' => array('id in (?)', $productor), 'order' => 'renglon_productivo_id'));
-            $ren = extract_prop($ren, 'renglon_productivo_id');
-            //var_dump($ren);
-            //$ren = $ren->renglon_productivo_id;
-            foreach($ren as $r){
-                $mun[$r-1] += 1;    
-            
-            
-
-            array_push($tabla, $mun);*/
-
-
-            foreach($ruats as $r){
-                $productor = Ruat::find('all', array('conditions' => array('id = ?', $r)));
-                $productor = $productor[0];
-                $productor = $productor->productor_id;
-                $ren = Productor::find('all', array('conditions' => array('id = ?', $productor), 'order' => 'renglon_productivo_id'));
-                $ren = $ren[0];
-                $ren = $ren->renglon_productivo_id;
-                $mun[$ren-1] += 1;
-            }
-            array_push($tabla, $mun);
-        }
-
-        for($i = 0 ; $i < count($tabla) ; $i++){
-            for($j = 0 ; $j < count($tabla[$i]) ; $j++){
-                $totalMunicipios[$i] += $tabla[$i][$j];
-                $total += $tabla[$i][$j];
-                $totalRenglon[$j] += $tabla[$i][$j];
-            }
-        }
-        /*var_dump($totalMunicipios);
-        var_dump($totalRenglon);*/
 
         $this->twiggy->set('renglones', $renglones);
         $this->twiggy->set('municipios', $municipios);
