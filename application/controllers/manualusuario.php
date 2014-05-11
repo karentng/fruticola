774bb33cc9818396b1fdac9ef33a15a960629a92
Karent Nathalia Narvaez <karentnathalia@gmail>
2014-05-10 21:17:12 -0500

    integracion manual de usuario

--- application/controllers/manualusuario.php
+++ application/controllers/manualusuario.php
@@ -14,23 +14,84 @@ class ManualUsuario extends CI_Controller {
         $this->twiggy->template("manual/andres");
         $this->twiggy->display();
     }
-    
-    public function index1(){
-        $this->twiggy->template("manual/eduar");
+
+    public function login()
+    {
+        $this->twiggy->template("manual/login");
         $this->twiggy->display();
     }
 
-    public function index2(){
+    public function listadoinicial()
+    {
+        $this->twiggy->template("manual/listadoinicial");
+        $this->twiggy->display();
+    }
+    
+    public function ruata()
+    {
+        $this->twiggy->template("manual/ruata");
+        $this->twiggy->display();
+    }
+
+    public function ruatb(){
         $this->twiggy->template("manual/ruat-b");
         $this->twiggy->display();
     }
 
-    public function index3(){
+    public function ruatc(){
+        $this->twiggy->template("manual/ruatc");
+        $this->twiggy->display();
+    }
+
+    public function ruatd(){
+        $this->twiggy->template("manual/ruatd");
+        $this->twiggy->display();
+    }
+
+    public function ubicacion(){
+        $this->twiggy->template("manual/ubicacion");
+        $this->twiggy->display();
+    }
+
+
+    public function suelos(){
+        $this->twiggy->template("manual/suelos");
+        $this->twiggy->display();
+    }
+
+
+    public function bpa(){
+        $this->twiggy->template("manual/bpa");
+        $this->twiggy->display();
+    }
+
+    public function comparativo(){
+        $this->twiggy->template("manual/comparativomunicipio");
+        $this->twiggy->display();
+    }
+
+    public function vtp(){
+        $this->twiggy->template("manual/vtp");
+        $this->twiggy->display();
+    }
+
+    public function indicadores(){
+        $this->twiggy->template("manual/indicadoresProduccion");
+        $this->twiggy->display();
+    }
+
+    public function estilosaprendizaje(){
+        $this->twiggy->template("manual/reporteEstilosAprendizaje");
+        $this->twiggy->display();
+    }
+
+
+    public function cosecha(){
         $this->twiggy->template("manual/cosecha");
         $this->twiggy->display();
     }
 
-    public function index4(){
+    public function perfilproductor(){
         $this->twiggy->template("manual/perfilproductor");
         $this->twiggy->display();
     }
